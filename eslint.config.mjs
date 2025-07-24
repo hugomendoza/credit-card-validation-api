3d66a751ea92d97c47a8618a0d480af35f32264a
Hugo <hugo_mendoza_s@outlook.com>
2025-07-24 12:53:28 -0500

    initial: init project configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.mjs'],
+  },
+  ...tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+    },
+  },
+  eslintConfigPrettier
+);
